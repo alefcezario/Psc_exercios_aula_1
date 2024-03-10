520f4cf4baa17142e1ac41c81f5b30b2140a68c1
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-03-10 01:06:02 -0300

    Create exercicio3.java

---
+++ exercicio3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exerciocio3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nome;
+        System.out.println("Olá, digite seu primeiro nome: ");
+        nome = input.nextLine(); 
+        System.out.println("\n Seja bem vindo ao aplicativo querido "+nome+" !");
+        
+        input.close(); 
+               
+         
+           
+
+    }
+}
